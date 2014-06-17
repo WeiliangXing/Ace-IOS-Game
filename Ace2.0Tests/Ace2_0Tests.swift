6e13cc9c565d148d44b6a8e3295367b37378c627
Weiliang Xing <xingwl0401@gmail.com>
2014-06-17 12:56:26 -0400

    Initial Commit

---
+++ Ace2.0Tests/Ace2_0Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Ace2_0Tests.swift
+//  Ace2.0Tests
+//
+//  Created by Weiliang Xing on 6/17/14.
+//  Copyright (c) 2014 Weiliang Xing. All rights reserved.
+//
+
+import XCTest
+
+class Ace2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
